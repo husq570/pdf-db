c3a5ca8e5726c56ce28e36ae5bf58d15352a9037
Vilius Andruška <andruska.vilius@gmail.com>
2021-02-25 18:12:18 +0200

    Added documents and home controller

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',[HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
